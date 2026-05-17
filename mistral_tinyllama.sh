815689a5807905b199c79c7c7766511c867bdedb
Sonnguyen4002 <nguyenhoangson041104@gmail.com>
2026-05-17 19:03:36 +0700

    first commit

---
+++ mistral_tinyllama.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+SEED=$1
+
+OUTPUT_DIR="./outputs/tiny_llama/seed-${SEED}"
+
+mkdir -p ${OUTPUT_DIR}
+
+OPTS=""
+
+# data .jsonl 
+OPTS+=" --train_data ./data/dolly/train.jsonl"
+OPTS+=" --val_data ./data/dolly/valid.jsonl"
+OPTS+=" --test_data ./data/dolly/valid.jsonl"
+
+# training
+OPTS+=" --num_train_epochs 1"
+OPTS+=" --batch_size 4"
+OPTS+=" --val_batch_size 32"
+OPTS+=" --learning_rate 1e-3"
+OPTS+=" --max_len 320"
+OPTS+=" --pad_to_multiple_of 1"
+
+# devices
+OPTS+=" --teach_device cuda:1"
+OPTS+=" --student_device cuda:1"
+
+# loss
+OPTS+=" --hard_label_loss_weight 0.5"
+OPTS+=" --span_loss True"
+OPTS+=" --span_weight_pooling True"
+OPTS+=" --span_loss_weight True"
+OPTS+=" --p 1.0"
+
+OPTS+=" --teacher_layers_mapping 28 30 32"
+OPTS+=" --student_encoder_layers_finetuned 18 20 22"
+OPTS+=" --n_encoder_finetuned 22"
+OPTS+=" --hidden_loss_weights 1 1 1"
+
+# models
+OPTS+=" --teacher_embedding_dimension 4096"
+OPTS+=" --output_dir ${OUTPUT_DIR}"
+OPTS+=" --teacher_model mistralai/Mistral-7B-v0.1"
+OPTS+=" --teacher_tokenizer mistralai/Mistral-7B-v0.1"
+OPTS+=" --student_model TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
+OPTS+=" --student_tokenizer TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
+
+# hf token
+OPTS+=" --hf_token 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"
+
+# extra arguments
+OPTS+=" --seed ${SEED}"
+# OPTS+=" --teacher_sft HoangTran223/MCW_KD_Teacher_Mistral7B"
+OPTS+=" --student_model_type tinyllama"
+OPTS+=" --teacher_model_type mistral"
+OPTS+=" --use_lora True"
+OPTS+=" --grad_accum_steps 4"
+
+# ==== run code====
+python run_distill_llm.py ${OPTS} >> ${OUTPUT_DIR}/train.log 2>&1
